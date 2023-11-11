6c99938e7c139affe08c9b374531d5e062d9e13a
1030pmy <1030pmy@gmail.com>
2023-11-12 00:23:22 +0900

    first commit

---
+++ src/main/java/com/project/metakakao/MetaKakaoApplication.java
@@ -0,0 +1,13 @@
+package com.project.metakakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaKakaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaKakaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/metakakao/MetaKakaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.metakakao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaKakaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
