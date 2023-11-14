21bde17963940ef190a3dec1befd6dae7d9dfe50
1030pmy <1030pmy@gmail.com>
2023-11-14 21:56:58 +0900

    Revert "question, user 생성"
    This reverts commit 338fe1a2771dbd3b66329efe4965b3b808474c0f.

--- src/main/java/com/project/metakakao/repository/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.metakakao.repository;
-
-import com.project.metakakao.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
